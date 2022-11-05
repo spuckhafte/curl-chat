8a3cf250bdbf890ad28fd7fb5adcabd607760609
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-11-05 09:45:27 +0530

    ::encrypt:: => data sent/recieved is encrypted

--- curl-client/index.js
+++ curl-client/index.js
@@ -1,6 +1,8 @@
 const { io } = require('socket.io-client')
+const enc = require('encdenc');
 const rl = require('serverline');
 rl.init();
+const Machine = new enc();
 
 const internals = [
     "/name",
@@ -13,7 +15,7 @@ const internals = [
 const Console = require('./helper/Console.js');
 new Console()
 
-const socket = io('https://curl-chat-production.up.railway.app/');
+const socket = io('http://localhost:3000');
 
 function run() {
     rl.getRL().question("> ", msg => {
@@ -26,54 +28,60 @@ function run() {
                     const cmdResponse = cmd[1].trim().toLowerCase();
                     if (internals.includes(cmdName)) {
                         if (cmdName == '/name') {
-                            socket.emit('setName', cmdResponse_RAW);
+                            socket.emit('setName', Machine.encrypt(cmdResponse_RAW));
                             run();
                         }
                         if (cmdName == '/expose') {
                             if (cmdResponse == '0' || cmdResponse == '1') {
-                                socket.emit('setExpose', cmdResponse);
+                                socket.emit('setExpose', Machine.encrypt(cmdResponse));
                                 run();
-                            } else socket.emit('msg-from-client', msg, socket.id);
+                            } else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
                         };
                         if (cmdName == '/whois') socket.emit('whois', cmdResponse_RAW);
                         if (cmdName == '/online') {
-                            if (cmdResponse == 'id' || cmdResponse == 'name') socket.emit('online', cmdResponse);
-                            else socket.emit('msg-from-client', msg, socket.id);
+                            if (cmdResponse == 'id' || cmdResponse == 'name') socket.emit('online', Machine.encrypt(cmdResponse));
+                            else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
                         };
                         if (cmdName == '/help') {
                             let help = "Commands:\n/name=user_name (set name)\n/expose=0|1 (hide/show name)\n/whois=user_id (find by id)\n/online=id|name"
                             console.log(help);
                             run();
                         }
-                    } else socket.emit('msg-from-client', msg, socket.id);
-                } else socket.emit('msg-from-client', msg, socket.id);
-            } else socket.emit('msg-from-client', msg, socket.id);
-        } else socket.emit('msg-from-client', msg, socket.id);
+                    } else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
+                } else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
+            } else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
+        } else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
     });
 }
 
 socket.on('verify', () => {
-    rl.getRL().question("Welcome to Curl Chat!\n/help => see inbuilt commands\npress enter to continue...", msg => {
+    rl.getRL().question("Welcome to Curl Chat!\n/help => see inbuilt commands\npress enter to continue...", _ => {
         socket.emit('verified')
     });
 })
 
-socket.on('connected', id => {
+socket.on('connected', (id, password) => {
     console.log(`[${id}${socket.id == id ? '-you connectced]' : ' connected]'}`);
+    password = JSON.parse(password);
+    Machine.config = password;
     run();
 });
 
 socket.on('msg-from-server', (msg, from) => {
+    msg = Machine.decrypt(msg);
     if (from != socket.id) console.log(`[${from}]: ${msg}`);
     run();
 });
 
 socket.on('user-found', (username, id) => {
+    username = Machine.decrypt(username);
     console.log(`[${id}: ${username}]`);
     run();
 })
 
 socket.on('online-found', (list, total) => {
+    list = Machine.decrypt(list);
+    total = Machine.decrypt(total);
     console.log(`(${total})[${list}]`);
     run();
 })
--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const http = require('http').createServer()
 const io = require('socket.io')(http, {
     cors: { origin: "*" }
 });
+const enc = require('encdenc');
+const Machine = new enc();
+require('dotenv').config();
+
+Machine.config = JSON.parse(process.env.CONFIG);
 
 const users = {};
 
@@ -11,33 +16,35 @@ io.on('connection', socket => {
 
     socket.on('verified', () => {
         users[socket.id] = ['default_name', true] // username, exposed?
-        io.emit('connected', socket.id);
+        io.emit('connected', socket.id, `[${Machine.config.toString()}]`);
     })
 
     socket.on('msg-from-client', msg => {
         io.emit('msg-from-server', msg, socket.id)
     });
     socket.on('setName', name => {
-        users[socket.id][0] = name;
+        users[socket.id][0] = Machine.decrypt(name);
     });
     socket.on('setExpose', expose => {
+        expose = Machine.decrypt(expose);
         users[socket.id][1] = expose == 0 ? false : true;
     });
     socket.on('whois', id => {
         socket.emit('user-found',
-            users[id] ?
+            Machine.encrypt(users[id] ?
                 users[id][1] ? socket.id == id ? `"${users[id][0]}"` : users[id][0]
                     : `HIDDEN`
-                : `DNE`, id
+                : `DNE`), id
         );
     });
     socket.on('online', type => {
+        type = Machine.decrypt(type);
         if (type == 'id') {
-            const list = Object.keys(users).map(id => id == socket.id ? `"${id}"` : id).toString();
-            socket.emit('online-found', list, Object.keys(users).length);
+            const list = Machine.encrypt(Object.keys(users).map(id => id == socket.id ? `"${id}"` : id).toString());
+            socket.emit('online-found', list, Machine.encrypt(Object.keys(users).length.toString()));
         } else {
-            const list = Object.keys(users).filter(id => users[id][1]).map(id => id == socket.id ? `"${users[id][0]}"` : users[id][0]).toString();
-            socket.emit('online-found', list, Object.keys(users).length);
+            const list = Machine.encrypt(Object.keys(users).filter(id => users[id][1]).map(id => id == socket.id ? `"${users[id][0]}"` : users[id][0]).toString());
+            socket.emit('online-found', list, Machine.encrypt(`${Object.keys(users).length}`));
         };
     })
 
