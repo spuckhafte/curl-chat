c98e789cf1eb24eab3946356b08617de33ae1861
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-11-05 23:06:39 +0530

    ::rooms::

--- curl-client/index.js
+++ curl-client/index.js
@@ -3,14 +3,26 @@ const enc = require('encdenc');
 const rl = require('serverline');
 rl.init();
 const Machine = new enc();
+const { v4 } = require('uuid');
 
 const internals = [
     "/name",
     "/expose",
     '/whois',
     '/online',
-    "/help"
+    "/help",
+    "/create",
+    "/join",
+    "/leave",
+    "/kick",
+    "/me"
 ]
+
+let room = null;
+let nameSet = false;
+let name = 'default_name'
+let verified = false;
+let expose = true;
 
 const Console = require('./helper/Console.js');
 new Console()
@@ -28,22 +40,82 @@ function run() {
                     const cmdResponse = cmd[1].trim().toLowerCase();
                     if (internals.includes(cmdName)) {
                         if (cmdName == '/name') {
+                            nameSet = cmdResponse_RAW != 'default_name';
+                            name = cmdResponse_RAW;
                             socket.emit('setName', Machine.encrypt(cmdResponse_RAW));
                             run();
                         }
                         if (cmdName == '/expose') {
                             if (cmdResponse == '0' || cmdResponse == '1') {
                                 socket.emit('setExpose', Machine.encrypt(cmdResponse));
+                                expose = cmdResponse == '1' ? true : false;
                                 run();
                             } else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
                         };
                         if (cmdName == '/whois') socket.emit('whois', cmdResponse_RAW);
                         if (cmdName == '/online') {
-                            if (cmdResponse == 'id' || cmdResponse == 'name') socket.emit('online', Machine.encrypt(cmdResponse));
+                            if (cmdResponse == 'id' || cmdResponse == 'name') socket.emit(
+                                'online', Machine.encrypt(cmdResponse), room ? true : false, room ? Machine.encrypt(room.id) : null
+                            );
                             else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
                         };
+                        if (cmdName == '/create') {
+                            if (!room) {
+                                const responseArray = cmdResponse.split('/'); // total_members/show(id or name)
+                                if (responseArray.length == 2) {
+                                    const [max, show] = responseArray;
+                                    if ((!isNaN(max)) && ['id', 'name'].includes(show)) {
+                                        if (show == 'name' && !nameSet) {
+                                            console.log("[for 'name' type room, /name should be defined]");
+                                            run();
+                                            return;
+                                        };
+                                        room = {
+                                            id: v4(), max, show,
+                                            joinee: socket.id,
+                                            host: socket.id
+                                        };
+                                        let roomData = Machine.encrypt(JSON.stringify(room));
+                                        socket.emit('join-room', roomData);
+                                    } else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
+                                } else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
+                            } else {
+                                console.log('[already in a room]');
+                                run();
+                            }
+                        };
+                        if (cmdName == '/join') {
+                            if (!room) {
+                                room = {
+                                    id: cmdResponse_RAW, nameSet
+                                };
+                                const roomData = Machine.encrypt(JSON.stringify(room));
+                                socket.emit('join-room', roomData);
+                            } else {
+                                console.log('[already in a room]');
+                                run();
+                            }
+                        };
+                        if (cmdName == '/leave') {
+                            if (room) socket.emit('leave-room');
+                            else socket.emit('msg-from-client', Machine.encrypt(msg), socket.id);
+                        }
+                        if (cmdName == '/kick') {
+                            if (room && room.host) {
+                                if (cmdResponse_RAW != socket.id) socket.emit('kick-user', Machine.encrypt(cmdResponse_RAW));
+                                else {
+                                    console.log("[CAN'T KICK YOURSELF]");
+                                    run();
+                                }
+                            }
+                            else socket.emit('msg-from-client', Machine.encrypt(msg + (room && !room.host ? ' [ONLY HOST]' : '')), socket.id);
+                        }
+                        if (cmdName == '/me') {
+                            console.log(`Data:\nid: ${socket.id}\nname: ${name}\nexposed: ${expose ? 'yes' : 'no'}\nroom: ${room ? room.id : 'global'}`);
+                            run();
+                        }
                         if (cmdName == '/help') {
-                            let help = "Commands:\n/name=user_name (set name)\n/expose=0|1 (hide/show name)\n/whois=user_id (find by id)\n/online=id|name"
+                            let help = "Commands:\n/name=user_name (set name)\n/expose=0|1 (hide/show name)\n/whois=user_id (find by id)\n/online=id|name\n/create={max_mem}/{show_type(id|name)} (prvt room, show your name or id there)\n/join=room_id (join room)\n/kick=user_id (only for host, kick user)\n/leave= (leave room)\n/me= (your data)"
                             console.log(help);
                             run();
                         }
@@ -55,21 +127,29 @@ function run() {
 }
 
 socket.on('verify', () => {
-    rl.getRL().question("Welcome to Curl Chat!\n/help => see inbuilt commands\npress enter to continue...", _ => {
+    rl.getRL().question("Welcome to Curl Chat!\n/help => see inbuilt commands\n\npress enter to continue...", _ => {
+        verified = true;
         socket.emit('verified')
     });
 })
 
 socket.on('connected', (id, password) => {
+    if (room || !verified) return;
     console.log(`[${id}${socket.id == id ? '-you connectced]' : ' connected]'}`);
     password = JSON.parse(password);
     Machine.config = password;
     run();
 });
 
-socket.on('msg-from-server', (msg, from) => {
+socket.on('msg-from-server', (msg, from, scope, showType, fromName) => {
     msg = Machine.decrypt(msg);
-    if (from != socket.id) console.log(`[${from}]: ${msg}`);
+    if (from != socket.id) {
+        if (scope == 'global' || showType == 'id') {
+            if (room && scope == 'global') return;
+            console.log(`[${from}]: ${msg}`);
+        }
+        if (showType == 'name') console.log(`[${Machine.decrypt(fromName)}(${from.substr(0, 4)}...)]: ${msg}`);
+    }
     run();
 });
 
@@ -85,7 +165,53 @@ socket.on('online-found', (list, total) => {
     console.log(`(${total})[${list}]`);
     run();
 })
-socket.on('user-left', id => {
+
+socket.on('room-join', (id, showType, username) => {
+    username = Machine.decrypt(username);
+    console.log(`[${id}${showType == 'name' ? `(${username})` : ""} joined room: ${room.id}]`);
+    run();
+});
+
+socket.on('room-join-fail', err => {
+    console.log(`[${err}]`);
+    room = null;
+    run();
+})
+
+socket.on('user-left', (id) => {
+    if (room) return;
     console.log(`[${id} left]`);
     run();
+});
+
+socket.on('host-left-room', () => {
+    room = null;
+    console.log('[HOST LEFT-NOW GLOBAL]');
+    run();
+});
+
+socket.on('member-left-room', (userId, name) => {
+    name = Machine.decrypt(name);
+    console.log(`[${userId}(${name}) LEFT]`);
+    run();
+})
+
+socket.on('you-left-room', (kick) => {
+    room = null;
+    console.log(kick ? '[KICKED BY HOST-NOW GLOBAL]' : '[ROOM LEFT-NOW GLOBAL]');
+    run();
+})
+
+socket.on('set-name-fail', err => {
+    console.log(err);
+    nameSet = true;
+    run();
+});
+
+socket.on('kick-fail', err => {
+    console.log(err);
+    run();
+})
+socket.on('kick-from-client', userId => {
+    if (socket.id == userId) socket.emit('leave-room', 'kick-me');
 })
--- server/server.js
+++ server/server.js
@@ -4,26 +4,36 @@ const io = require('socket.io')(http, {
 });
 const enc = require('encdenc');
 const Machine = new enc();
+const { v4 } = require('uuid')
 require('dotenv').config();
 
 Machine.config = JSON.parse(process.env.CONFIG);
 
-const users = {};
+const users = {}; // { "user_id": [username, exposed, null] }
+const rooms = {}; // { "room_id": { room_id, max, show(id|name), host, members[user_id] } }
 
 io.on('connection', socket => {
     console.log(socket.id, 'connected');
     socket.emit('verify');
 
     socket.on('verified', () => {
-        users[socket.id] = ['default_name', true] // username, exposed?
+        users[socket.id] = ['default_name', true, null]
         io.emit('connected', socket.id, `[${Machine.config.toString()}]`);
     })
 
-    socket.on('msg-from-client', msg => {
-        io.emit('msg-from-server', msg, socket.id)
+    socket.on('msg-from-client', msg => { // msg -> already encerypted (client: enc(msg) => server => all_clients: dec(msg))
+        let roomId = users[socket.id][2];
+        let to = roomId ? 'room' : 'global';
+        if (to == 'global') io.emit('msg-from-server', msg, socket.id, 'global');
+        else io.to(roomId).emit('msg-from-server', msg, socket.id, 'room', rooms[roomId].show, Machine.encrypt(users[socket.id][0]));
     });
     socket.on('setName', name => {
-        users[socket.id][0] = Machine.decrypt(name);
+        name = Machine.decrypt(name);
+        if (users[socket.id][2] && rooms[users[socket.id][2]].show == 'name' && name == 'default_name') {
+            socket.emit('set-name-fail', "[name can't be 'default_name' in 'name' type room]")
+            return;
+        }
+        users[socket.id][0] = name;
     });
     socket.on('setExpose', expose => {
         expose = Machine.decrypt(expose);
@@ -37,14 +47,114 @@ io.on('connection', socket => {
                 : `DNE`), id
         );
     });
-    socket.on('online', type => {
+    socket.on('online', (type, forRoom, id) => { // id -> roomId
         type = Machine.decrypt(type);
-        if (type == 'id') {
-            const list = Machine.encrypt(Object.keys(users).map(id => id == socket.id ? `"${id}"` : id).toString());
-            socket.emit('online-found', list, Machine.encrypt(Object.keys(users).length.toString()));
+        if (forRoom) {
+            id = Machine.decrypt(id);
+            if (type == 'id') {
+                const list = Machine.encrypt(rooms[id].members.map(userId => userId == socket.id ? `"${userId}"` : userId).toString()) // id
+                socket.emit('online-found', list, Machine.encrypt(rooms[id].members.length.toString()));
+            } else {
+                let showType = rooms[id].show; //showtype of room
+                let list;
+
+                if (showType == 'id') list = Machine.encrypt(rooms[id].members.filter(userId => users[userId][1]).map(userId => userId == socket.id ? `"${users[userId][0]}"` : users[userId][0]).toString()); // name (hid)
+                else list = Machine.encrypt(rooms[id].members.map(userId => userId == socket.id ? `"${users[userId][0]}"` : users[userId][0]).toString()); // name (unhid)
+                socket.emit('online-found', list, Machine.encrypt(rooms[id].members.length.toString()));
+            }
         } else {
-            const list = Machine.encrypt(Object.keys(users).filter(id => users[id][1]).map(id => id == socket.id ? `"${users[id][0]}"` : users[id][0]).toString());
-            socket.emit('online-found', list, Machine.encrypt(`${Object.keys(users).length}`));
+            if (type == 'id') {
+                const list = Machine.encrypt(Object.keys(users).map(userId => userId == socket.id ? `"${userId}"` : userId).toString());
+                socket.emit('online-found', list, Machine.encrypt(Object.keys(users).length.toString()));
+            } else {
+                const list = Machine.encrypt(Object.keys(users).filter(userId => users[userId][1]).map(userId => userId == socket.id ? `"${users[userId][0]}"` : users[userId][0]).toString());
+                socket.emit('online-found', list, Machine.encrypt(`${Object.keys(users).length}`));
+            };
+        }
+    })
+
+    socket.on('join-room', roomData => {
+        roomData = JSON.parse(Machine.decrypt(roomData));
+        let status = roomData.host ? 'create' : 'join';
+        if (status == 'create') {
+            rooms[roomData.id] = {
+                ...roomData,
+                count: 1,
+                members: [roomData.joinee],
+            };
+            delete rooms[roomData.id].joinee;
+            users[socket.id][2] = roomData.id;
+            socket.join(roomData.id)
+            io.to(roomData.id).emit('room-join', socket.id, roomData.show, Machine.encrypt(users[socket.id][0]));
+        } else {
+            if (!rooms[roomData.id]) {
+                socket.emit('room-join-fail', 'ROOM DNE');
+                return;
+            }
+            if (rooms[roomData.id].count == rooms[roomData.id].max) {
+                socket.emit('room-join-fail', 'ROOM FULL');
+                return;
+            }
+            if (rooms[roomData.id].show == 'name' && !roomData.nameSet) {
+                socket.emit('room-join-fail', "for 'name' type room, /name should be defined");
+                return;
+            }
+            rooms[roomData.id].count = rooms[roomData.id].count + 1;
+            rooms[roomData.id].members.push(socket.id);
+            users[socket.id][2] = roomData.id;
+            socket.join(roomData.id);
+            io.to(roomData.id).emit('room-join', socket.id, rooms[roomData.id].show, Machine.encrypt(users[socket.id][0]));
+        };
+    })
+
+    socket.on('kick-user', async userId => {
+        userId = Machine.decrypt(userId);
+        const room = users[socket.id][2];
+        if (!rooms[room].members.includes(userId)) {
+            socket.emit('kick-fail', '[USER DNE]');
+            return;
+        };
+
+        socket.to(room).emit('kick-from-client', userId);
+    })
+
+    socket.on('leave-room', (kick) => {
+        const room = users[socket.id][2];
+        if (room) {
+            const wasHost = rooms[room].host == socket.id;
+            if (wasHost) {
+                socket.leave(room);
+                socket.to(room).emit('host-left-room');
+                io.in(room).socketsLeave(room);
+                rooms[room].members.forEach(member => users[member][2] = null);
+                delete rooms[room];
+                socket.emit('you-left-room');
+            } else {
+                socket.leave(room);
+                users[socket.id][2] = null;
+                rooms[room].count = rooms[room].count - 1;
+                rooms[room].members = rooms[room].members.filter(userId => userId != socket.id);
+                socket.to(room).emit('member-left-room', socket.id, Machine.encrypt(rooms[room].show == 'name' ? users[socket.id][0] : 'default_name'));
+                if (kick) socket.emit('you-left-room', 'kick');
+                else socket.emit('you-left-room');
+            };
+        };
+    });
+
+    socket.on('disconnecting', () => {
+        if (!users[socket.id]) return;
+        const room = users[socket.id][2];
+        if (room) {
+            const wasHost = rooms[room].host == socket.id;
+            if (wasHost) {
+                socket.to(room).emit('host-left-room');
+                io.in(room).socketsLeave(room);
+                delete rooms[room];
+            } else {
+                socket.to(room).emit('member-left-room', socket.id, Machine.encrypt(rooms[room].show == 'name' ? users[socket.id][0] : 'default_name'));
+                rooms[room].count = rooms[room].count - 1;
+                rooms[room].members = rooms[room].members.filter(userId => userId != socket.id);
+            };
         };
     })
 
@@ -55,6 +165,6 @@ io.on('connection', socket => {
     });
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => console.log(PORT + '*'));
 
